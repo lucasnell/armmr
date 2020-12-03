ba5a9a18d38721fb2f7888fe690519b8100b77f3
Lucas Nell <lucas@lucasnell.com>
2020-12-03 14:10:58 -0600

    Added bayesian_se fxn, split summary and print, fixef returns vector

--- R/armmMod_class.R
+++ R/armmMod_class.R
@@ -33,75 +33,58 @@ new_armmMod <- function(.stan, .call, .hmc, .x_means_sds, .y_means_sds, .stan_da
 #' @noRd
 #'
 print.armmMod <- function(x, digits = max(3, getOption("digits") - 3), ...) {
-    summary(x, digits = digits, ...)
-}
-
-#' Summary for an `armmMod` object.
-#'
-#' Note that `Lower` and `Upper` fields are from 95% HPDIs.
-#'
-#' @export
-#'
-#' @method summary armmMod
-#'
-#' @noRd
-#'
-summary.armmMod <- function(object,
-                            digits = max(3, getOption("digits") - 3),
-                            ...) {
-
     cat("Autoregressive mixed model\n")
     cat(sprintf("  method:         %s\n",
-                ifelse(object$hmc, "Hamiltonian Monte Carlo",
+                ifelse(x$hmc, "Hamiltonian Monte Carlo",
                        "Direct optimization")))
-    cat(sprintf("  family:         %s\n", family(object)))
-    form <- if (inherits(object$call$formula, "formula")) {
-        object$call$formula
+    cat(sprintf("  family:         %s\n", family(x)))
+    form <- if (inherits(x$call$formula, "formula")) {
+        x$call$formula
     } else {
-        eval(object$call$formula, parent.frame(1L))
+        eval(x$call$formula, parent.frame(1L))
     }
     cat("  formula:       ", paste(trimws(deparse(form)), collapse = " "), "\n")
-    cat("  data:          ", paste(trimws(deparse(object$call$data)),
+    cat("  data:          ", paste(trimws(deparse(x$call$data)),
                                    collapse = " "))
     scale_strs <- c()
-    if (!is.null(object$x_means_sds)) scale_strs <- c(scale_strs, "scaled X")
-    if (!is.null(object$y_means_sds)) scale_strs <- c(scale_strs, "scaled Y")
+    if (!is.null(x$x_means_sds)) scale_strs <- c(scale_strs, "scaled X")
+    if (!is.null(x$y_means_sds)) scale_strs <- c(scale_strs, "scaled Y")
     if (length(scale_strs) > 0) {
         cat(sprintf(" << %s >>\n", paste(scale_strs, collapse = " & ")))
     } else cat("\n")
-    cat("  observations:  ", nobs(object), "\n")
-    if (!is.null(object$call$rstan_control)) {
+    cat("  observations:  ", nobs(x), "\n")
+    if (!is.null(x$call$rstan_control)) {
         cat("  rstan options: ", gsub("list\\(|\\)", "",
-                                      deparse(object$call$rstan_control)), "\n")
+                                      deparse(x$call$rstan_control)), "\n")
     }
-    cat(sprintf("  obs. error:     %s\n", "sig_obs" %in% names(object$stan)))
+    cat(sprintf("  obs. error:     %s\n", "sig_obs" %in% names(x$stan)))
     cat("------\n")
 
-    LL <- median(rstan::extract(object$stan, "log_lik_sum")[[1]])
+    LL <- median(rstan::extract(x$stan, "log_lik_sum")[[1]])
     cat("Median posterior logLik:", LL, "\n")
-    loo_obj <- tryCatch(loo.armmMod(object),
-                     warning = function(w) {
-                         return(list(loo = suppressWarnings(loo.armmMod(object)),
-                                     warn = w))
-                     })
+    loo_obj <- tryCatch(loo(x),
+                        warning = function(w) {
+                            return(list(loo = suppressWarnings(loo(x)),
+                                        warn = w))
+                        })
     if (inherits(loo_obj, "list")) {
         loo_est <- loo_obj[["loo"]][["estimates"]]["looic","Estimate"]
         loo_warn <- paste0("** loo warning: ",
-                          trimws(loo_obj[["warn"]][["message"]]), "\n")
+                           trimws(loo_obj[["warn"]][["message"]]), "\n")
     } else {
         loo_est <- loo_obj[["estimates"]]["looic","Estimate"]
         loo_warn <- ""
     }
 
-    waic_obj <- tryCatch(waic.armmMod(object),
-                  warning = function(w) {
-                      return(list(waic = suppressWarnings(waic.armmMod(object)),
-                                  warn = w))
-                  })
+    waic_obj <- tryCatch(waic(x),
+                         warning = function(w) {
+                             return(list(waic = suppressWarnings(waic(x)),
+                                         warn = w))
+                         })
     if (inherits(waic_obj, "list")) {
         waic_est <- waic_obj[["waic"]][["estimates"]]["waic","Estimate"]
         waic_warn <- paste0("** waic warning: ",
-                          trimws(waic_obj[["warn"]][["message"]]), "\n")
+                            trimws(waic_obj[["warn"]][["message"]]), "\n")
 
     } else {
         waic_est <- waic_obj[["estimates"]]["waic","Estimate"]
@@ -110,6 +93,34 @@ summary.armmMod <- function(object,
 
     print(c(LOO = loo_est, WAIC = waic_est), digits = digits)
     cat(loo_warn, waic_warn, sep = "")
+
+    invisible(NULL)
+}
+
+#' Summary for an `armmMod` object.
+#'
+#' Standard errors are based on either quantiles or HPDIs,
+#' and are half the width of the 68% uncertainty interval.
+#'
+#' @param se_method A single string, for either using quantiles (`"quantile"`)
+#'     or HPDI (`"hpdi"`) to compute the standard errors.
+#'     Defaults to `"quantile"`.
+#'
+#' @export
+#'
+#' @method summary armmMod
+#'
+#'
+summary.armmMod <- function(object,
+                            se_method = c("quantile", "hpdi"),
+                            digits = max(3, getOption("digits") - 3),
+                            ...) {
+
+
+    print(object, digits = digits)
+
+    se_method <- match.arg(tolower(se_method), c("quantile", "hpdi"))
+
     cat("------\n")
 
     AR <- autoreg(object)
@@ -127,9 +138,7 @@ summary.armmMod <- function(object,
 
     cat("------\n")
     cat("Fixed effects:\n")
-    FE <- fixef(object)
-    rownames(FE) <- paste0(" ", rownames(FE))
-    print(FE, digits = digits)
+    print_fixef_w_se(object, se_method, digits)
 
     invisible(NULL)
 
@@ -183,6 +192,28 @@ waic.armmMod <- function(x, ...) {
 }
 
 
+
+#
+# Print fixed effects coefficients and standard errors.
+# This is used in the summary method
+#
+print_fixef_w_se <- function(object, se_method, digits) {
+
+    A <- rstan::extract(object$stan, "alpha")[[1]]
+
+    SEs <- bayesian_se(A, se_method)
+
+    fixef_df <- data.frame(Median = apply(A, 2, median),
+                           `Std.Error` = SEs)
+
+    rownames(fixef_df) <- paste0(" ", colnames(object$stan_data$x))
+
+    print(fixef_df, digits = digits)
+
+    invisible(NULL)
+}
+
+
 #
 # This is for use in the summary method.
 #
@@ -201,7 +232,34 @@ print_sigma_betas <- function(object, digits) {
                          c("", sigmaB_df$Groups[-nrow(sigmaB_df)])] <- ""
 
     print(sigmaB_df, row.names = FALSE, right = FALSE, digits = digits)
+
+    invisible(NULL)
 }
+
+
+
+# Estimate Bayesian standard errors by computing half the width of the
+# 68% CI.
+# This CI can be calculated using quantiles or HPDI
+# Used multiple times below.
+bayesian_se <- function(x, se_method) {
+    if (!isTRUE(length(dim(x)) == 2)) {
+        stop("\n`x` arg to `bayesian_se` should be 2-dimensional.")
+    }
+    if (se_method == "quantile") {
+        upper <- unname(sapply(1:ncol(x), function(i) quantile(x[,i], 0.84)))
+        lower <- unname(sapply(1:ncol(x), function(i) quantile(x[,i], 0.16)))
+    } else if (se_method == "hpdi") {
+        ints <- lapply(1:ncol(x), function(i) hpdi(x[,i], 0.68))
+        upper <- sapply(ints, function(x) x[["upper"]])
+        lower <- sapply(ints, function(x) x[["lower"]])
+    } else stop("\n`se_method` arg to `bayesian_se` should be `\"quantile\"` or",
+                "`\"hpdi\"`")
+    SEs <- 0.5 * (upper - lower)
+    return(SEs)
+}
+
+
 
 
 
@@ -235,32 +293,24 @@ autoreg <- function(object, ...) {
     UseMethod("autoreg")
 }
 #' @export
-#' @param method A single string, for either using quantiles (`"quantile"`)
+#' @param se_method A single string, for either using quantiles (`"quantile"`)
 #'     or HPDI (`"hpdi"`) to compute the standard errors.
 #'     Defaults to `"quantile"`.
 #' @method autoreg armmMod
 #' @describeIn autoreg Autoregressive parameters for an `armmMod` object
 autoreg.armmMod <- function(object,
-                            method = c("quantile", "hpdi"),
+                            se_method = c("quantile", "hpdi"),
                             ...) {
 
     if (is.null(eval(object$call$ar_form))) return(NULL)
 
-    method <- match.arg(tolower(method), c("quantile", "hpdi"))
+    se_method <- match.arg(tolower(se_method), c("quantile", "hpdi"))
     # Add `+0` to get all levels of groups:
     new_form <- as.formula(paste0(deparse(eval(object$call$ar_form)), "+0"))
     ar_names <- colnames(model.matrix(new_form, eval(object$call$data)))
     phis <- rstan::extract(object$stan, "phi")[[1]]
 
-    if (method == "quantile") {
-        upper <- unname(sapply(1:ncol(phis), function(i) quantile(phis[,i], 0.84)))
-        lower <- unname(sapply(1:ncol(phis), function(i) quantile(phis[,i], 0.16)))
-    } else {
-        ints <- lapply(1:ncol(phis), function(i) hpdi(phis[,i], 0.68))
-        upper <- sapply(ints, function(x) x[["upper"]])
-        lower <- sapply(ints, function(x) x[["lower"]])
-    }
-    SEs <- 0.5 * (upper - lower)
+    SEs <- bayesian_se(phis, se_method)
 
     autoreg_df <- data.frame(Median = apply(phis, 2, median),
                              `Std.Error` = SEs)
@@ -277,37 +327,23 @@ autoreg.armmMod <- function(object,
 #' @aliases fixef fixed.effects fixef.armmMod
 #' @docType methods
 #' @param object A fitted model with class `armmMod`.
-#' @param method A single string, for either using quantiles (`"quantile"`)
-#'     or HPDI (`"hpdi"`) to compute the standard errors.
-#'     Defaults to `"quantile"`.
 #' @param ... Ignored.
-#' @return A dataframe of fixed-effects estimates. Standard errors are
-#'     based on either quantiles or HPDIs, and are half the width of
-#'     the 68% uncertainty interval.
+#' @return A vector of fixed-effects estimates.
 #' @importFrom lme4 fixef
 #' @export fixef
 #' @method fixef armmMod
 #' @export
-fixef.armmMod <- function(object, method = c("quantile", "hpdi"), ...) {
+fixef.armmMod <- function(object, ...) {
 
-    method <- match.arg(tolower(method), c("quantile", "hpdi"))
+    A <- apply(rstan::extract(object$stan, "alpha")[[1]], 2, median)
 
-    A <- rstan::extract(object$stan, "alpha")[[1]]
+    names(A) <- colnames(object$stan_data$x)
 
-    if (method == "quantile") {
-        upper <- unname(sapply(1:ncol(A), function(i) quantile(A[,i], 0.84)))
-        lower <- unname(sapply(1:ncol(A), function(i) quantile(A[,i], 0.16)))
-    } else {
-        ints <- lapply(1:ncol(A), function(i) hpdi(A[,i], 0.68))
-        upper <- sapply(ints, function(x) x[["upper"]])
-        lower <- sapply(ints, function(x) x[["lower"]])
-    }
-    SEs <- 0.5 * (upper - lower)
+    return(A)
+}
 
-    fixef_df <- data.frame(Median = apply(A, 2, median),
-                           `Std.Error` = SEs)
 
-    rownames(fixef_df) <- colnames(object$stan_data$x)
+
 
     return(fixef_df)
 }
